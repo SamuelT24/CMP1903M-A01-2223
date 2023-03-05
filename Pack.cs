31e98849f07e4bc94bd17c83fe4c1a7028a079e6
Samuel T <samueltracey24@yahoo.com>
2023-03-05 15:46:16 +0000

    Add code for the 'deal' and 'dealCard' functions, add stub return to the 'shuffleCardPack' function

--- Pack.cs
+++ Pack.cs
@@ -18,16 +18,44 @@ namespace CMP1903M_A01_2223
         public static bool shuffleCardPack(int typeOfShuffle)
         {
             //Shuffles the pack based on the type of shuffle
-
+            return false; // stub, just putting this here so the program can actually run
         }
         public static Card deal()
         {
             //Deals one card
+            Pack p = new Pack();
+            var random = new Random();
+            int randomCardIndex = random.Next(p.pack.Count);
+            return p.pack[randomCardIndex];
 
         }
         public static List<Card> dealCard(int amount)
         {
             //Deals the number of cards specified by 'amount'
+            Pack p = new Pack();
+            var random = new Random();
+            List<Card> chosenCards = new List<Card>();
+            List<int> previousIndexes = new List<int>();
+            for (int i = 0; i < amount; i++)
+            {
+                bool invalidCardIndex = true;
+                int randomCardIndex = random.Next(p.pack.Count);
+                while (invalidCardIndex)
+                {
+                    if (previousIndexes.Contains(randomCardIndex))
+                    {
+                        randomCardIndex = random.Next(p.pack.Count);
+                    }
+                    else
+                    {
+                        invalidCardIndex = false;
+                    }
+                }
+                previousIndexes.Add(randomCardIndex);
+                chosenCards.Add(p.pack[randomCardIndex]);
+            }
+
+            return chosenCards;
         }
     }
 }
